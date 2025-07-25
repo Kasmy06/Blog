7ae3bb3c8c6456e4ee34db0f48ca98434280ba80
Amani Samuel <amani3.kouadio@uvci.edu.ci>
2025-07-25 17:18:06 +0000

    Seeder commit

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -16,8 +17,19 @@ class PostFactory extends Factory
      */
     public function definition()
     {
+        $title =  fake()->unique->sentence;
+        $content = fake()->paragraphs(asText: true);
+        $created_at = fake()->dateTimeBetween('-1 year');
+
         return [
-            //
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'excerpt' => Str::limit($content, 150),
+            'content' => $content,
+            'thumbnail' => fake()->imageUrl,
+            'created_at' =>  $created_at,
+            'updated_at' =>  $created_at,
+
         ];
     }
 }
